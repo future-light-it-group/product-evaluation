61b8f3677aaeeabed12849a84a004d30f9134b44
Le Bao Triet <crist.azn@gmail.com>
2013-02-06 18:46:34 +0700

    add test line from develop

--- sources/font-end/views/site/index.php
+++ sources/font-end/views/site/index.php
@@ -7,7 +7,7 @@ $this->pageTitle=Yii::app()->name;
 ?>
 
 <h1>Welcome to <i><?php echo CHtml::encode(Yii::app()->name); ?> Front-end side</i></h1>
-
+<p>Test from develop branch</p>
 <p>Congratulations! You have successfully created your Yii application.</p>
 
 <p>You may change the content of this page by modifying the following two files:</p>
